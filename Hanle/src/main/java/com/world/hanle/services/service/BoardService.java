6229797d4f683da0308751c091b4d5e3638e25f6
ehnawh <ehnawh@gmail.com>
2014-11-19 15:45:55 +0900

    test 주석처리

---
+++ Hanle/src/main/java/com/world/hanle/services/service/BoardService.java
@@ -0,0 +1,10 @@
+package com.world.hanle.services.service;
+
+import org.springframework.stereotype.Service;
+
+import com.world.hanle.services.model.Board;
+
+@Service
+public class BoardService extends GenericService<Board> {
+
+}
