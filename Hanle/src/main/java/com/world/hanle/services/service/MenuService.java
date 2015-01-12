5261d75caefa7638a116d0f594b8c124737f7d83
ehnawh <ehnawh@gmail.com>
2015-01-12 22:41:01 +0900

    tt

---
+++ Hanle/src/main/java/com/world/hanle/services/service/MenuService.java
@@ -0,0 +1,13 @@
+package com.world.hanle.services.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.world.hanle.services.model.Menu;
+
+@Service
+@Transactional
+public class MenuService extends GenericService<Menu> {
+	
+}
